31aba96b8cc1999613184911438d15366d32bd31
Brady Joslin <accounts@bradyjoslin.com>
2019-04-07 19:58:15 -0500

    Update player.py
    Add comments identifying albums in dictionary

--- player.py
+++ player.py
@@ -26,12 +26,12 @@ class Player:
 player = Player("", False, None)
 
 albums = {
-    "143 122 166 137 218": "6DEjYFkNZh67HP7R9PSZvv",
-    "241 161 49 45 76": "2QJmrSgbdM35R67eoGQo4j",
-    "1 109 143 46 205": "6sUzNE1SPNLBXBCZs3PIAO",
-    "241 134 198 45 156": "6EVYTRG1drKdO8OnIQBeEj",
-    "241 117 60 45 149": "5MQBzs5YlZlE28mD9yUItn",
-    "67 187 202 46 28": "0UlbGi4oAth8s6rwaGSU8Z",
+    "143 122 166 137 218": "6DEjYFkNZh67HP7R9PSZvv", # Reptation by Taylor Swift
+    "241 161 49 45 76": "2QJmrSgbdM35R67eoGQo4j",    # 1989 by Taylor Swift
+    "1 109 143 46 205": "6sUzNE1SPNLBXBCZs3PIAO",    # thank u, next by Ariana Grande
+    "241 134 198 45 156": "6EVYTRG1drKdO8OnIQBeEj",  # Me Everything by Ariana Grande
+    "241 117 60 45 149": "5MQBzs5YlZlE28mD9yUItn",   # Prism by Katy Perry
+    "67 187 202 46 28": "0UlbGi4oAth8s6rwaGSU8Z",    # Witness by Katy Perry
 }
 
 while True:
